645579cdd4516c7524b533079db0d69a0c8a2406
Vu Dinh <vdinh@redhat.com>
2015-08-18 14:19:17 -0400

    Thumbtack SimpleDatabase 08/18/2015
    Signed-off-by: Vu Dinh <vdinh@redhat.com>

---
+++ src/Thumbtack/Program.java
@@ -0,0 +1,111 @@
+package Thumbtack;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Program {
+	
+	// The database to be used
+	protected static SimpleDatabase db;
+	
+	/*
+	 * The main program to run the database
+	 * The program can either read commands from text file or from stdIn
+	 */
+	public static void main(String[] args) {
+		// Check to see there are any parameters in command line
+		if (args.length > 0) {
+			try {
+				// File Input and Reader to read text file
+				FileInputStream in = new FileInputStream(args[0]);
+				BufferedReader br = new BufferedReader(new InputStreamReader(in));
+				// Input string
+				String str;
+				// Command array
+				String[] arr;
+				// Initialize the database
+				db = new SimpleDatabase();
+				// Read the file line by line
+				while ((str = br.readLine()) != null) {
+					// Parse the command
+					arr = str.split(" ");
+					// Print command
+					System.out.println(str);
+					// Check if END command
+					if (arr[0].equals("END")) {
+						// Exit program if END command is used
+						break;
+					} else
+						// Pass command string to commandHandler method
+						commandHandler(arr);
+				}
+				// Close BufferedReader and InputStream
+				br.close();
+				in.close();
+
+			} catch (IOException e) {
+				System.err.println("File is not found.");
+			}
+		} else {
+			// Scanner for input
+			Scanner input = new Scanner(System.in);
+			String str;
+			String[] arr;
+			// Initialize database
+			db = new SimpleDatabase();
+			// Wait for new input
+			while (input.hasNext()) {
+				// Get the input
+				str = input.nextLine();
+				// Parse input to get command
+				arr = str.split(" ");
+				if (arr[0].equals("END")) {
+					break;
+				} else
+					commandHandler(arr);
+			}
+			// Close scanner input
+			input.close();
+		}
+	}
+
+	/*
+	 * A method to handle different commands for the database
+	 */
+	private static void commandHandler(String[] arr) {
+		if (arr[0].equals("BEGIN")) {
+			db.begin();
+		} else if (arr[0].equals("GET")) {
+			if (arr.length == 2) {
+				db.get(arr[1]);
+			} else
+				System.out.println("INVALID COMMAND");
+		} else if (arr[0].equals("SET")) {
+			if (arr.length == 3) {
+				db.set(arr[1], arr[2]);
+			} else
+				System.out.println("INVALID COMMAND");
+		} else if (arr[0].equals("UNSET")) {
+			if (arr.length == 2) {
+				db.unset(arr[1]);
+			} else
+				System.out.println("INVALID COMMAND");
+		} else if (arr[0].equals("NUMEQUALTO")) {
+			if (arr.length == 2) {
+				db.numEqualTo(arr[1]);
+				;
+			} else
+				System.out.println("INVALID COMMAND");
+		} else if (arr[0].equals("ROLLBACK")) {
+			db.rollback();
+		} else if (arr[0].equals("COMMIT")) {
+			db.commit();
+			;
+		} else {
+			System.out.println("INVALID COMMAND");
+		}
+	}
+}
--- src/Thumbtack/SimpleDatabase.java
+++ src/Thumbtack/SimpleDatabase.java
@@ -1,75 +1,202 @@
 package Thumbtack;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.Map;
+import java.util.Set;
 
+/*
+ * A simple in-memory database named SimpleDatabase
+ */
 public class SimpleDatabase {
-	
-	private HashMap<String,LinkedList<Integer>> db;
-	
+
+	// Map to hold variables and their values
+	private Map<String, LinkedList<String>> database;
+	// Command log to keep track of BEGIN, SET and UNSET commands
+	private LinkedList<String> log;
+	// Set to keep track of modified variables
+	private Set<String> change;
+	// Indicate of transaction blocks are in process
+	private boolean isBegin;
+	// Count number of transaction blocks
+	private int nestedCounter;
+
+	/*
+	 * SimpleDatabase Constructor Initialize fields
+	 */
 	public SimpleDatabase() {
-		db = new HashMap<String,LinkedList<Integer>>();
+		database = new HashMap<String, LinkedList<String>>();
+		log = new LinkedList<String>();
+		change = new HashSet<String>();
+		isBegin = false;
+		nestedCounter = 0;
 	}
-	
-	public static void main(String[] args) {
-		if (args.length > 0) {
-			try {
-				FileInputStream in = new FileInputStream(args[0]);
-				BufferedReader br = new BufferedReader(new InputStreamReader(in));
-				String str;
-				while ((str = br.readLine()) != null)
-				{
-					
-				}
-				
+
+	/*
+	 * Activate the transaction block
+	 */
+	public void begin() {
+		// Flag isBegin
+		if (isBegin == false)
+			isBegin = true;
+		// Increment transaction block counter
+		nestedCounter++;
+		// Add to log
+		log.add("BEGIN");
+	}
+
+	/*
+	 * Set a variable to a value
+	 * 
+	 * @param name: name of variable
+	 * 
+	 * @param value: value to set
+	 */
+	public void set(String name, String value) {
+		// If transaction block is process
+		if (isBegin == true) {
+			// Keep track of commands and variable changes
+			log.add(name);
+			change.add(name);
+			// If variable already exits
+			if (database.containsKey(name)) {
+				database.get(name).add(value);
+			} else {
+				// Add new variable to database
+				LinkedList<String> newList = new LinkedList<String>();
+				newList.add(value);
+				database.put(name, newList);
 			}
-			catch(IOException e) {
-				e.printStackTrace();
+		} else {
+			if (database.containsKey(name)) {
+				database.get(name).removeLast();
+				database.get(name).add(value);
+			} else {
+				LinkedList<String> newList = new LinkedList<String>();
+				newList.add(value);
+				database.put(name, newList);
 			}
 		}
-		else {
-			Scanner input = new Scanner(System.in);
-			while (input.hasNext()) {
-				
-			}
-		}
 	}
-	
-	public void set(String name, int value) {
-		if (db.containsKey(name)) {
-			db.put(name, db.get(name).add(value));
-			
-		}
+
+	/*
+	 * Get a variable to a value
+	 * 
+	 * @param name: name of variable
+	 */
+	public void get(String name) {
+		// Check if variable exits in DB
+		if (database.containsKey(name)) {
+			if (database.get(name).isEmpty()) {
+				System.out.println("NULL");
+			} else
+				System.out.println(database.get(name).getLast());
+		} else
+			// Print NULL if variable doesn't exit in database
+			System.out.println("NULL");
 	}
-	
-	public int get(String name) {
-		return 0;
-	}
-	
+
+	/*
+	 * Unset a variable to a value
+	 * 
+	 * @param name: name of variable
+	 */
 	public void unset(String name) {
-		
+		// If transaction block is in process
+		if (isBegin == true) {
+			// Keep track of commands and variable changes
+			change.add(name);
+			log.add(name);
+			// Add NULL to variable's list of values
+			if (database.containsKey(name)) {
+				database.get(name).add("NULL");
+			}
+		} else {
+			if (database.containsKey(name)) {
+				// Remove the last entry and add NULL
+				database.get(name).removeLast();
+				database.get(name).add("NULL");
+			}
+		}
 	}
-	
-	public void numEqualTo(int num) {
-		
+
+	/*
+	 * Count the number of variable equals to value
+	 * 
+	 * @param value: name of value to search
+	 */
+	public void numEqualTo(String num) {
+		int counter = 0;
+		// Go through the map and check if the any variables equal to value
+		for (Map.Entry<String, LinkedList<String>> entry : database.entrySet()) {
+			if (entry.getValue().getLast().equals(num)) {
+				// Increment counter if found
+				counter++;
+			}
+		}
+		// Print out the counter
+		System.out.println(counter);
 	}
-	
-	public void end() {
-		
-	}
-	
+
+	/*
+	 * Rollback the changes in the most recent
+	 * transaction block. 
+	 */
 	public void rollback() {
-		
-		// TODO
+		// If no block is in process, print NO TRANSACTION
+		if (isBegin == false) {
+			System.out.println("NO TRANSACTION");
+		} else {
+			String temp;
+			while (nestedCounter > 0) {
+				// Pop the command log one by one
+				temp = log.removeLast();
+				// If the command log entry is BEGIN, then we are done
+				if (temp.equals("BEGIN")) {
+					// Decrement nestedCounter
+					nestedCounter--;
+					// If nestedCounter is 0, all block are done
+					// So de-flag the isBegin
+					if (nestedCounter == 0)
+						isBegin = false;
+					break;
+				} else {
+					// Remove the last entry in variable's list of value
+					database.get(temp).removeLast();
+				}
+			}
+		}
 	}
-	
+
+	/*
+	 * Commit the changes in all open transaction blocks. 
+	 */
 	public void commit() {
-		
+		// Check the command log has any transactions
+		if (log.isEmpty()) {
+			System.out.println("NO TRANSACTION");
+		} else {
+			// Return all fields back to default values
+			isBegin = false;
+			nestedCounter = 0;
+			// Discard the old log and get a new log
+			log = new LinkedList<String>();
+			// Go through the change log to commit the modified variables
+			for (String str : change) {
+				// Replace the list of values with a new list of only committed value
+				LinkedList<String> newList = new LinkedList<String>();
+				newList.add(database.get(str).getLast());
+				database.put(str, newList);
+			}
+			change = new HashSet<String>();
+		}
+
+		/*
+		 * Now, the map (database) is updated.
+		 * You can save/transmit database somewhere else for storage or backup.
+		 * Otherwise, database is lost after JAVA program exits.
+		 */
 	}
 
 }
